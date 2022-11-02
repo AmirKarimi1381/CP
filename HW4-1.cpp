99922fb88ddcd1c437e5618f2be2e769a79a49f0
Amir <48445959+AmirKarimi1381@users.noreply.github.com>
2022-11-02 13:13:46 -0700

    40013841054181

---
+++ HW4-1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float a,b,jam,tafrig,zarb,tagsim;
+	cout << "Enter Number one: \n";
+	cin >> a;
+	cout << "Enter Number two: \n";
+	cin >> b;
+	jam = a+b;
+	cout <<"jam: " << jam << "\n";
+	tafrig = a-b;
+	cout <<"tafrig: "<< tafrig << "\n";
+	zarb = a*b;
+	cout <<"zarb: "<< zarb << "\n";
+	tagsim = a/b;
+	cout <<"tagsim: "<< tagsim << "\n";
+	
+// baghi mande dar zaban c & c++ ba motagayere FLOAT emkan pazir nist
+	return 0;
+}
