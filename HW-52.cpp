5aad0539056e54da39b6e320e306f556987abce0
Amir <48445959+AmirKarimi1381@users.noreply.github.com>
2022-11-13 13:19:30 -0800

    Add files via upload
    40013841054181

---
+++ HW-52.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char n;
+	cout << "Pleas Enter Any Keyboard Buttons: ";
+	cin >> n;
+	
+	switch (n){
+	
+    case 'a':
+    	cout << "VOWEL";
+        break;
+
+    case 'e':
+        cout << "VOWEL";
+        break;
+    
+    case 'i':
+        cout << "VOWEL";
+        break;
+        
+    case 'o':
+        cout << "VOWEL";
+        break;
+        
+    case 'u':
+        cout << "VOWEL";
+        break;
+        
+	default:
+        cout << "This Is Not VOWEL Character";
+}
+	return 0;
+}
